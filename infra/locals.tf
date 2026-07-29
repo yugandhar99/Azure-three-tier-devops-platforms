57c9f1da8daafeea2c56e717a3566d09db4bf76b
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-07-29 16:28:18 -0500

    Update locals.tf

--- infra/locals.tf
+++ infra/locals.tf
@@ -4,4 +4,4 @@ locals {
   locations             = toset(compact([var.primary_location, var.secondary_location]))
   extra_naming_suffixes = { for l in local.locations : l => [local.project_name, terraform.workspace, l] }
   db_password_version   = 1
-}
+} 
