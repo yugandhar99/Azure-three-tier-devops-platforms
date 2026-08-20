1cb3602b6db0a8b90d1c2042eec7b2de72a6e0c4
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-08-20 14:20:39 -0500

    Update locals.tf

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  project_name          = "three-tier-app"
+  project_name          = "three-tier-app" 
   vnet_cidr             = "10.254.0.0/16"
   locations             = toset(compact([var.primary_location, var.secondary_location]))
   extra_naming_suffixes = { for l in local.locations : l => [local.project_name, terraform.workspace, l] }
